b3679da53d5e167b0926c560217ac4cf15485dee
Carlos <adiz98@hotmail.com>
2018-10-08 20:28:33 -0500

    Modificaciones

--- AdminFree/src/main/java/adminfree/AdminFreeApplication.java
+++ AdminFree/src/main/java/adminfree/AdminFreeApplication.java
@@ -3,6 +3,13 @@ package adminfree;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * Clase RAIZ donde se desplega la aplicación
+ * 
+ * @author Carlos Andres Diaz
+ *
+ */
 @SpringBootApplication
 public class AdminFreeApplication {
 
