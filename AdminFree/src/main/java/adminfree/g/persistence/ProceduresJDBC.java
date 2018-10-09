fbe4d38b908f1b96c86397774b33d94b683729d8
Carlos <adiz98@hotmail.com>
2018-10-09 11:11:20 -0500

    Commit

---
+++ AdminFree/src/main/java/adminfree/g/persistence/ProceduresJDBC.java
@@ -0,0 +1,12 @@
+package adminfree.g.persistence;
+
+/**
+ * 
+ * Clase que contiene los llamados de los procedimientos almacenados del sistema
+ * 
+ * @author Carlos Andres Diaz
+ *
+ */
+public class ProceduresJDBC {
+
+}
