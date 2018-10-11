b2ede6cac13ab9a12a99cda82b50d71534700fe6
Carlos <adiz98@hotmail.com>
2018-10-11 17:16:26 -0500

    commit

---
+++ AdminFree/src/test/java/adminfree/configuraciones/ActivarCliente.java
@@ -0,0 +1,47 @@
+package adminfree.configuraciones;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import adminfree.b.services.ConfiguracionesService;
+import adminfree.d.model.configuraciones.ClienteDTO;
+
+/**
+ * Test para el servicio ConfiguracionesService-activarCliente
+ * 
+ * @author Carlos Andres Diaz
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivarCliente {
+
+	/** Service que contiene las configuraciones del sistema */
+	@Autowired
+	private ConfiguracionesService configuracionesService;
+
+	/**
+	 * Test que permite activar un Cliente
+	 */
+	@Test
+	public void activarCliente() {
+		try {
+			// se configura el CLIENTE ACTIVAR
+			ClienteDTO clienteActivar = new ClienteDTO();
+			clienteActivar.setId(1L);
+
+			// se invoca el llamado del servicio para ACTIVAR el cliente
+			this.configuracionesService.activarCliente(clienteActivar);
+
+			// si llega a esta punto es porque todo fue procesado correctamente
+			assertTrue(true);
+		} catch (Exception e) {
+			assertTrue(false);
+		}
+	}
+}
