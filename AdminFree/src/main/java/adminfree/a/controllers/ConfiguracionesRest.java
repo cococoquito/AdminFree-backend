93ddad797445d893fd4e0d2ca4bffb45ad25f75f
Carlos <adiz98@hotmail.com>
2018-10-12 15:20:46 -0500

    COMMIT

--- AdminFree/src/main/java/adminfree/a/controllers/ConfiguracionesRest.java
+++ AdminFree/src/main/java/adminfree/a/controllers/ConfiguracionesRest.java
@@ -1,12 +1,46 @@
 package adminfree.a.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import adminfree.b.services.ConfiguracionesService;
+import adminfree.e.utilities.Constants;
+
 /**
  * 
  * Clase que contiene todos los servicios REST para el modulo de Configuraciones
+ * localhost:puerto/Constants.CONFIGURACIONES_NOMBRE_API/
  * 
  * @author Carlos Andres Diaz
  *
  */
+@RestController
+@RequestMapping(Constants.CONFIGURACIONES_NOMBRE_API)
 public class ConfiguracionesRest {
 
+	/** Service que contiene las configuraciones del sistema */
+	@Autowired
+	private ConfiguracionesService configuracionesService;
+
+	/**
+	 * Servicio REST que permite obtener los CLIENTES del sistema
+	 * 
+	 * @return lista de CLIENTES parametrizados en el sistema
+	 */
+	@RequestMapping(
+			value = Constants.GET_CLIENTES, 
+			method = RequestMethod.GET, 
+			produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseEntity<Object> getClientes() {
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(this.configuracionesService.listarClientes());
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+	}
 }
--- AdminFree/src/main/java/adminfree/e/utilities/Constants.java
+++ AdminFree/src/main/java/adminfree/e/utilities/Constants.java
@@ -8,15 +8,16 @@ package adminfree.e.utilities;
  *
  */
 public class Constants {
-
-	/**
-	 * Constant that represent the name for the API REST of the application
-	 */
-	public static final String API = "api";
-
-	/**
-	 * Constant that represent the character to grant the permits for all request of
-	 * the application WEB
-	 */
+	
+	/** Constant that represent the character to grant the permits for all request of the application WEB */
 	public static final String GRANTS_PERMITS = "/**";
+	
+	/** Constant that represent the message of error technical for the REST services */
+	public static final String MESSAGE_ERROR_TECHNICAL = "Se produjo un error en el sistema:";
+	
+	/** Constantes para los nombre de los servicios REST del modulo de configuraciones*/
+	public static final String CONFIGURACIONES_NOMBRE_API = "configuracionesapi";
+	public static final String GET_CLIENTES = "clientes";
+	
+	
 }
