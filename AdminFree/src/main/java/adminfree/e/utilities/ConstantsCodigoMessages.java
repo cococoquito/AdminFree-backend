d0b3f6c0a68884c03a448aa7031dc1c8e2060ece
Carlos <adiz98@hotmail.com>
2018-10-15 13:32:15 -0500

    commit

--- AdminFree/src/main/java/adminfree/e/utilities/ConstantsCodigoMessages.java
+++ AdminFree/src/main/java/adminfree/e/utilities/ConstantsCodigoMessages.java
@@ -10,10 +10,10 @@ public class ConstantsCodigoMessages {
 
 	/************* CODIGOS PARA LOS ERRORES DE NEGOCIO, RESPONSE ERROR 400 *************/
 	/** 01 - El Usuario y la Contraseña que usted ingresó no ha sido reconocido. Por favor, inténtelo de nuevo. */
-	public static final String COD_AUTENTICACION_FALLIDA = "01";	
+	public static final String COD_AUTENTICACION_FALLIDA = "1";	
 	
 	
 	/************* CODIGOS PARA LOS ERRORES TECNICO, RESPONSE ERROR 500 ****************/
 	/** 01 - Se produjo un error en el sistema: */
-	public static final String COD_ERROR_TECHNICAL = "01";
+	public static final String COD_ERROR_TECHNICAL = "1";
 }
