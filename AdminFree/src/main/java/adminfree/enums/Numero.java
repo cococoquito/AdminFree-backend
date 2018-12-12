20229b8f314b1540eb92b3f12417e095f098bfe6
Carlos <adiz98@hotmail.com>
2018-12-11 20:09:35 -0500

    ajustea nuemro

--- AdminFree/src/main/java/adminfree/enums/Numero.java
+++ AdminFree/src/main/java/adminfree/enums/Numero.java
@@ -9,8 +9,14 @@ package adminfree.enums;
  */
 public enum Numero {
 
-	ZERO(0), UNO(1), DOS(2), TRES(3), CUATRO(4), CINCO(5), SEIS(6), SIETE(7), OCHO(8), NUEVE(9), DIEZ(10), ONCE(11), TREINTA(30),
-	MIL(1000), DOCE(12);
+	ZERO(0), UNO(1), DOS(2),
+	TRES(3), CUATRO(4), CINCO(5),
+	SEIS(6), SIETE(7), OCHO(8),
+	NUEVE(9), DIEZ(10), ONCE(11),
+	DOCE(12), TRECE(13), CATORCE(14),
+	QUINCE(15), DIECISEIS(16), DIECISIETE(17),
+	DIECIOCHO(18), DIECINUEVE(19), VEINTE(20),
+	TREINTA(30), MIL(1000);
 
 	public final Integer value;
 
