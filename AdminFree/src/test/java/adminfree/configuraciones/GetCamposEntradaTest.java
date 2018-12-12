3faad7db23d2e808923e9cfabbe06718952b90ff
Carlos <adiz98@hotmail.com>
2018-12-12 12:20:52 -0500

    commit

---
+++ AdminFree/src/test/java/adminfree/configuraciones/GetCamposEntradaTest.java
@@ -0,0 +1,46 @@
+package adminfree.configuraciones;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import adminfree.dtos.configuraciones.CampoEntradaDTO;
+import adminfree.services.ConfiguracionesService;
+
+/**
+ * Test para el servicio ConfiguracionesService.getCamposEntrada
+ *
+ * @author Carlos Andres Diaz
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GetCamposEntradaTest {
+
+	/** Service que contiene las configuraciones del sistema */
+	@Autowired
+	private ConfiguracionesService configuracionesService;
+
+	/**
+	 * Test que permite obtener los campos de entrada de informacion asociado a un cliente
+	 */
+	@Test
+	public void getCamposEntrada() {
+		try {
+			Long idCliente = 1l;
+			List<CampoEntradaDTO> campos = this.configuracionesService.getCamposEntrada(idCliente);
+
+			// debe existir campos asociadas al cliente
+			assertTrue(campos != null && !campos.isEmpty());
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			assertTrue(false);
+		}
+	}
+}
