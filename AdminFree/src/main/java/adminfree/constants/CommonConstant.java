a62037d103f72fda59e22d98ca0df47cd263eacd
Carlos <adiz98@hotmail.com>
2019-03-05 15:50:15 -0500

    ajustas

--- AdminFree/src/main/java/adminfree/constants/CommonConstant.java
+++ AdminFree/src/main/java/adminfree/constants/CommonConstant.java
@@ -24,6 +24,7 @@ public class CommonConstant {
 	public static final String LAST_INSERT_ID = "SELECT LAST_INSERT_ID()";
 	public static final Integer ID_ADMINISTRADOR = -1;
 	public static final String ADMINISTRADOR = "Administrador";
+	public static final String FORMATO_FECHA_MSYQL = "%d/%M/%Y";
 
 	/** Constantes para las restricciones de los campos */
 	public static final String KEY_CAMPO_UNICO_NOMENCLATURA = "3";
