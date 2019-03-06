4cce0e36f82c1e0278325520dad2f329b73fa3f9
Carlos <adiz98@hotmail.com>
2019-03-06 17:34:18 -0500

    ajsutes

---
+++ AdminFree/src/test/java/adminfree/correspondencia/DescargarDocumentoTest.java
@@ -0,0 +1,48 @@
+package adminfree.correspondencia;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import adminfree.dtos.correspondencia.DocumentoDTO;
+import adminfree.services.CorrespondenciaService;
+
+/**
+ * Test para el servicio CorrespondenciaService.descargarDocumento
+ * 
+ * @author Carlos Andres Diaz
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DescargarDocumentoTest {
+
+	/** Objecto que contiene los servicios relacionados modulo correspondencia */
+	@Autowired
+	private CorrespondenciaService correspondenciaService;
+
+	/**
+	 * Test para la descarga de un documento de correspondencia
+	 */
+	@Test
+	public void descargarDocumento() {
+		try {
+			// identificadores necesarios para la descarga
+			String idCliente = "1";
+			String idDocumento = "1";
+
+			// se procede a descargar el documento
+			DocumentoDTO documento = this.correspondenciaService.descargarDocumento(idCliente, idDocumento);
+
+			// debe existir el documento
+			assertTrue(documento != null && documento.getContenido().length > 0);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			assertTrue(false);
+		}
+	}
+}
