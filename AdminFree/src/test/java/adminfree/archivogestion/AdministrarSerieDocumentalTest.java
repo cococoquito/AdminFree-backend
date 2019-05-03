9d149e23fefff74dec7f4ad6aa6f7bd341bba5b1
Carlos <adiz98@hotmail.com>
2019-05-03 11:20:43 -0500

    ajutes

--- AdminFree/src/test/java/adminfree/archivogestion/AdministrarSerieDocumentalTest.java
+++ AdminFree/src/test/java/adminfree/archivogestion/AdministrarSerieDocumentalTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import adminfree.constants.TipoEvento;
 import adminfree.dtos.archivogestion.SerieDocumentalDTO;
+import adminfree.dtos.archivogestion.TipoDocumentalDTO;
 import adminfree.services.ArchivoGestionService;
 
 /**
@@ -36,16 +37,27 @@ public class AdministrarSerieDocumentalTest {
 			SerieDocumentalDTO serie = new SerieDocumentalDTO();
 			serie.setTipoEvento(TipoEvento.CREAR);
 			serie.setIdCliente(1);
-			serie.setCodigo("25.2.1");
-			serie.setNombre("ACTAS");
+			serie.setCodigo("90.0.1");
+			serie.setNombre("HOJAS DE VIDA");
 			serie.setAG(1);
 			serie.setAC(0);
 			serie.setCT(true);
 			serie.setM(true);
 			serie.setS(true);
 			serie.setE(true);
-			serie.setProcedimiento("Este es el procedimiento de la serie DE ACTAS");
+			serie.setProcedimiento("Este es el procedimiento DE HV");
 			serie.setIdUsuarioCreacion(1);
+
+			// se agregan los tipos documentales
+			TipoDocumentalDTO doc1 = new TipoDocumentalDTO();
+			doc1.setId(1);
+			TipoDocumentalDTO doc2 = new TipoDocumentalDTO();
+			doc2.setId(5);
+			TipoDocumentalDTO doc3 = new TipoDocumentalDTO();
+			doc3.setId(3);
+			serie.agregarTipoDocumental(doc1);
+			serie.agregarTipoDocumental(doc2);
+			serie.agregarTipoDocumental(doc3);
 			this.archivoGestionService.administrarSerieDocumental(serie);
 
 			// test para la edicion de la serie
@@ -62,12 +74,13 @@ public class AdministrarSerieDocumentalTest {
 			editar.setS(false);
 			editar.setE(false);
 			editar.setProcedimiento("procedimiento editado");
+			editar.agregarTipoDocumental(doc2);
 			this.archivoGestionService.administrarSerieDocumental(editar);
 
 			// test para eliminar una serie
 			SerieDocumentalDTO eliminar = new SerieDocumentalDTO();
 			eliminar.setTipoEvento(TipoEvento.ELIMINAR);
-			eliminar.setIdSerie(1L);
+			eliminar.setIdSerie(19L);
 			this.archivoGestionService.administrarSerieDocumental(eliminar);
 
 			// si llega a este punto es porque todos los procesos se ejucutaron sin problemas
